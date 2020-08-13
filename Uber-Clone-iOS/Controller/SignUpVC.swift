8d602d234c3890babd9de36ebc1452fda975e106
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-08-13 19:08:15 +0300

    Added firebase pods

--- Uber-Clone-iOS/Controller/SignUpVC.swift
+++ Uber-Clone-iOS/Controller/SignUpVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class SignUpVC: UIViewController {
     
@@ -57,6 +58,7 @@ class SignUpVC: UIViewController {
     private let signUpButton: AuthButton = {
         let button = AuthButton(type: .system)
         button.setTitle("Log In", for: .normal)
+        button.addTarget(self, action: #selector(registerPressed), for: .touchUpInside)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         return button
     }()
@@ -64,7 +66,7 @@ class SignUpVC: UIViewController {
     private let segmentedControl: UISegmentedControl = {
         let segmentedControl = UISegmentedControl(items: ["Rider", "Driver"])
         segmentedControl.selectedSegmentIndex = 0
-
+        
         segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.white], for: .normal)
         segmentedControl.setTitleTextAttributes([NSAttributedString.Key.foregroundColor : UIColor.black], for: .selected)
         
@@ -72,17 +74,19 @@ class SignUpVC: UIViewController {
     }()
     
     private let signInButton: UIButton = {
-           let button = UIButton(type: .system)
-           let title = NSMutableAttributedString(string: "Already have account? ", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray,
-               NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16)])
-           title.append(NSAttributedString(string: "Sign In!", attributes:
-               [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
-               NSAttributedString.Key.foregroundColor : UIColor.blueTint]))
-           button.setAttributedTitle(title, for: .normal)
-           button.addTarget(self, action: #selector(signInPressed), for: .touchUpInside)
-           return button
-       }()
-
+        let button = UIButton(type: .system)
+        let title = NSMutableAttributedString(string: "Already have account? ", attributes: [NSAttributedString.Key.foregroundColor : UIColor.lightGray,
+                                                                                             NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16)])
+        title.append(NSAttributedString(string: "Sign In!", attributes:
+            [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 16),
+             NSAttributedString.Key.foregroundColor : UIColor.blueTint]))
+        button.setAttributedTitle(title, for: .normal)
+        button.addTarget(self, action: #selector(signInPressed), for: .touchUpInside)
+        return button
+    }()
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavBar()
@@ -111,9 +115,29 @@ class SignUpVC: UIViewController {
         view.addSubview(signInButton)
         signInButton.anchor(right: view.rightAnchor, bottom: view.safeAreaLayoutGuide.bottomAnchor, left: view.leftAnchor, rightPadding: 16, bottomPadding: 16, leftPadding: 16, height: 44)
     }
-
+    
+    // MARK: - Selectors
+    
     @objc func signInPressed() {
         navigationController?.popViewController(animated: true)
     }
-
+    
+    @objc func registerPressed() {
+        guard let email = emailTextField.text, let password = passwordTextField.text, let fullName = fullNameTextField.text else { return }
+        let accountType = segmentedControl.selectedSegmentIndex
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            if let error = error {
+                print("Error occured while registering user \(error)")
+                return
+            }
+            
+            guard let uid = result?.user.uid else { return }
+            
+            let values = ["email": email, "fullName": fullName, "accountType": accountType] as [String : Any]
+            
+            Database.database().reference().child("users").child(uid).updateChildValues(values, withCompletionBlock: { _, _ in
+                print("Successfully created user")
+            })
+        }
+    }
 }
