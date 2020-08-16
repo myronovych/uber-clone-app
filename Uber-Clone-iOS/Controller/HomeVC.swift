c5f50aaff5b0f597449997799b0099e01e520f39
Oleksandr Myronovych <alexmyronovych@gmail.com>
2020-08-16 21:56:38 +0300

    tablecell/tableview ui

--- Uber-Clone-iOS/Controller/HomeVC.swift
+++ Uber-Clone-iOS/Controller/HomeVC.swift
@@ -167,8 +167,16 @@ extension HomeVC: LocationInpuViewDelegate {
 }
 
 extension HomeVC: UITableViewDataSource, UITableViewDelegate {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Header"
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return section == 0 ? 2 : 5
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
--- Uber-Clone-iOS/View/DestinationTableViewCell.swift
+++ Uber-Clone-iOS/View/DestinationTableViewCell.swift
@@ -11,10 +11,30 @@ import UIKit
 class DestinationTableViewCell: UITableViewCell {
     
     static let reuseId = "destinationCell"
+    
+    private let mainLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Khreschatyk 1"
+        label.font.withSize(14)
+        return label
+    }()
 
+    private let secondaryLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Khreschatyk 1, Kyiv, Ukraine"
+        label.font.withSize(14)
+        label.textColor = .lightGray
+        return label
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
+        let stack = UIStackView(arrangedSubviews: [mainLabel, secondaryLabel])
+        stack.axis = .vertical
+        stack.spacing = 4
+        addSubview(stack)
+        stack.anchor(left: leftAnchor, leftPadding: 12)
+        stack.centerY(inView: self)
     }
     
     required init?(coder: NSCoder) {
